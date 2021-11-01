704ba809779e692b2bce5760b15582383fc629a9
sihyung92 <fjzjqhdl@gmail.com>
2021-11-01 21:58:15 +0900

    feat: 더미 api에 더미 이미지가 나가도록 수정

--- backend/src/main/java/wooteco/prolog/image/ui/ImageController.java
+++ backend/src/main/java/wooteco/prolog/image/ui/ImageController.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import wooteco.prolog.image.application.ImageService;
 import wooteco.prolog.image.application.dto.ImageResponse;
+import wooteco.prolog.image.domain.Image;
 
 @RestController
 @RequestMapping("/image")
 @RequiredArgsConstructor
 public class ImageController {
 
-
     @PostMapping
     public ResponseEntity<ImageResponse> uploadImage(@RequestPart MultipartFile image) throws IOException {
-        return ResponseEntity.ok(new ImageResponse("imageUrl"));
+        return ResponseEntity.ok(new ImageResponse("https://dmaxaug2ve9od.cloudfront.net/promotion_banner/jujeol-promotion-cheers.png"));
     }
-
 }
